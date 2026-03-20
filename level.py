06e70b3893d1cc8976350cea4e8ebfd4925ac900
professor212-pro <em.kouassi.28@eigsica.ma>
2026-03-20 11:57:28 +0000

    level.py

---
+++ level.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+
+class Level:
+    def __init__(self, width: int = 800, height: int = 600) -> None:
+        self.width = width
+        self.height = height
+
